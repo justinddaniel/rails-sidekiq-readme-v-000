4eb4da0dd85fd55de9989efa380b99d3e3f4967e
Justin Daniel <justinddaniel@gmail.com>
2018-02-05 23:23:31 +0000

    automatically backed up by learn

--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -4,5 +4,6 @@ class LeadsWorker
   def perform(leads_file)
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
   end
 end
